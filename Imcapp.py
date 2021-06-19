877fb1c4a0d986d7b3dd4be5494714de4103ad52
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-06-19 13:24:13 -0500

    Update Imcapp.py

--- Imcapp.py
+++ Imcapp.py
@@ -1,4 +1,5 @@
 
+from typing import Text
 import streamlit as st
 st.image("logo.jpeg")
 
@@ -20,7 +21,25 @@ if butace==True:
     imc=peso/talla**2
     imc2=int(imc)
     st.subheader(imc2)
-    if imc2 
+    if imc2 <=18.5:
+        st.subheader("Estas en bajo peso")
+    elif imc2 =>18.5 and <24.9:
+        st.subheader("Peso normal, no requieres intervención quirúrgica")
+    elif imc2 >25 and <29.9:
+        st.subheader("Sobrepeso, se requiere manejo médico y control de habitos dieteticos")
+    elif imc2 >29.9 and <34.9:
+        st.subheader("Obesidad grado I")
+        comorb=st.checkbox("Tienes alguna enfermedad como Diabetes mellitus, hipertensión arterial, dislipidemias")
+        if comorb==True:
+            st.subheader("Eres candidato a cirugía bariátrica")
+        else:
+            st.subheader("Debes mantener un estilo de vida saludable, con dieta para reducción de peso y aumentar ejercicio físico")
+    elif imc2 >35 and <39.9:
+        st.subheader("Obesidad grado II")
+        st.write("Requieres manejo médico, nutricional y de hábitos, tambien eres candidato a cirugía batriátrica como la Manga gástrica (gastrectomia vertical")
+    elif imc2 >40:
+        st.subheader("Obesidad grado III")
+        st.write("Estas en alto riesgo de enfermedades metabolicas, y tu riesgo de muerte es mayor al de una persona sin obesidad, se sugiere que seas valorado por un cirujano bariatra y un equimo médico")
 
 
 #Agregar un if para el IMC mayor a 35 con comorbilidades
